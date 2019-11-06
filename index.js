bddfaa65ec5a1d3a714088a42a48e0d887631c6f
guillermo <guillermo@theblackbox.io>
2019-11-06 18:32:15 +0100

    v1.1 release

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ async function run() {
 
     const octokit = new github.GitHub(myToken);
     const titleRegex = core.getInput('issueRegex', {required: true});
-    core.debug(`Checking ${titleRegex} against the PR title`);
+    console.log(`Checking ${titleRegex} against the PR title`);
 
     const title = getPrTitle();
     console.log(`title: ${title}`);
@@ -20,14 +20,14 @@ async function run() {
       core.setFailed("PR title does not contain any issue ID");
     }
     const issueId = ext[0];
-    core.debug(`Found issue ID ${issueId}`);
+    console.log(`Found issue ID ${issueId}`);
     const ytUrl = core.getInput('youtrackUrl');
-    const body = ytUrl + ytUrl.endsWith("/") ? "" : "/" + "issue/" + issueId;
+    const body = ytUrl + (ytUrl.endsWith("/") ? "" : "/") + "issue/" + issueId;
     await octokit.issues.createComment({
       owner: github.context.issue.owner,
       repo: github.context.issue.repo,
       issue_number: github.context.issue.number,
-      body: body
+      body: body,
     });
     core.setOutput('issueId', issueId);
   } 
