12d1a44525c4e6f17f9dd208611ed88af42d5c4c
papeloto <vn4varro@gmail.com>
2020-03-12 11:55:18 +0100

    refactor: multiple issues

--- index.js
+++ index.js
@@ -1,135 +1,177 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const fetch = require('node-fetch');
+const core = require("@actions/core");
+const github = require("@actions/github");
+const axios = require("axios");
 
+const GITHUB_TOKEN = core.getInput("githubToken");
+const YT_TOKEN = core.getInput("youtrackToken");
+const YT_URL = core.getInput("youtrackUrl");
+const YT_ISSUE = "api/issues/";
+const REPO_URL = `https://github.com/${github.context.issue.owner}/${github.context.issue.repo}`;
+const PR_URL = `https://github.com/${github.context.issue.owner}/${github.context.issue.repo}/pull/${github.context.issue.number}`;
+const REGEX = core.getInput("issueRegex", { required: true });
+const ISSUE_REGEX = new RegExp(REGEX, "g");
 
-// most @actions toolkit packages have async methods
+const ytApi = axios.create({
+  headers: {
+    authorization: `Bearer ${YT_TOKEN}`,
+    accept: "application/json",
+    "cache-control": "no-cache",
+    "content-type": "application/json"
+  },
+  baseURL: YT_URL + (YT_URL.endsWith("/") ? "" : "/") + YT_ISSUE
+});
+
+const octokit = new github.GitHub(GITHUB_TOKEN);
+
 async function run() {
-  try {
-    const myToken = core.getInput('githubToken');
+  const tickets = getMatchingTickets();
 
-    const octokit = new github.GitHub(myToken);
-    const titleRegex = core.getInput('issueRegex', {required: true});
-    console.log(`Checking ${titleRegex} against the PR title`);
+  if (tickets.length === 0) {
+    throw "PR description does not contain any issue ID.";
+  }
 
-    const title = getPrTitle();
-    console.log(`title: ${title}`);
+  console.log(`Found issues: ${tickets.join(",")}.`);
 
-    const reg = new RegExp(titleRegex);
-    const ext = reg.exec(title);
-    if (ext === null) {
-      core.setFailed("PR title does not contain any issue ID");
-    }
-    const issueId = ext[0];
-    console.log(`Found issue ID ${issueId}`);
-    const ytUrl = core.getInput('youtrackUrl') + (core.getInput('youtrackUrl').endsWith("/") ? "" : "/");
-    const ytToken = core.getInput('youtrackToken');
-    const ytApiIssueUrl = ytUrl + 'api/issues/' + issueId;
-    const ytHeaders = {
-      "headers": {
-        "authorization": "Bearer " + ytToken,
-        "accept": "application/json",
-        "cache-control": "no-cache",
-        "content-type": "application/json"
-      }
-    };
-    await fetch(ytApiIssueUrl, {
-      "method": "GET",
-      ...ytHeaders
-    })
-      .then(async response => {
-        if (response.ok) {
-          const body = `PR attached to issue [${issueId}](${ytUrl}issue/${issueId})`;
-          await octokit.issues.createComment({
-            owner: github.context.issue.owner,
-            repo: github.context.issue.repo,
-            issue_number: github.context.issue.number,
-            body: body,
-          });
-          console.log(`Issue found in YT`);
-          const ytApiIssueCommentUrl = ytApiIssueUrl + '/comments';
-          const ytApiGetFields = ytApiIssueUrl + '/fields?fields=name,id,value(name)';
-          const ytApiChangeStateField = stateFieldId => ytApiIssueUrl + `/fields/${stateFieldId}?fields=name,id,value(name)`;
-          const repoUrl = `https://github.com/${github.context.issue.owner}/${github.context.issue.repo}`;
-          const pullUrl = `https://github.com/${github.context.issue.owner}/${github.context.issue.repo}/pull/${github.context.issue.number}`;
-          await fetch(ytApiIssueCommentUrl, {
-            "method": "POST",
-            ...ytHeaders,
-            body: JSON.stringify({
-              text: `New Pull Request [#${github.context.issue.number}](${pullUrl}) opened at [${github.context.issue.owner}/${github.context.issue.repo}](${repoUrl}) by ${github.context.actor}.`,
-              usesMarkdown: true,
-            })
-          }).then(response => {
-            console.log(`Comment stored in YT with status ${response.status}`);
-          }).catch(err => {
-              core.setFailed(err.message);
-            });
-          const fields = await fetch(ytApiGetFields, {
-            "method": "GET",
-            ...ytHeaders
-          }).then(response => {
-            console.log(`Got issue fields ${response.status}`);
-            return response.json();
-          }).catch(err => {
-            core.setFailed(err.message);
-          });
-          const currentState = fields.find(x => x.name === "State");
-          const currentStateValue = currentState && currentState.value && currentState.value.name.toLowerCase();
-          if (currentStateValue === 'to do' || currentStateValue === 'to fix' || currentStateValue === 'in progress') {
-            const statePayload = {
-              "value": {
-                "name": "PR Open"
-              }
-            };
-            await fetch(ytApiChangeStateField(currentState.id), {
-              "method": "POST",
-              ...ytHeaders,
-              body: JSON.stringify(statePayload)
-            }).then(response => {
-              console.log(`Changed issue to PR Open ${response.status}`);
-              const body = `Issue [${issueId}](${ytUrl}issue/${issueId}) changed from *${currentState.value.name}* to *PR Open*`;
-              octokit.issues.createComment({
-                owner: github.context.issue.owner,
-                repo: github.context.issue.repo,
-                issue_number: github.context.issue.number,
-                body: body,
-              });
-            }).catch(err => {
-              core.setFailed(err.message);
-            });
-          }
-          const currentType = fields.find(x => x.name === "Type");
-          if (currentType && currentType.value && currentType.value.name){
-            octokit.issues.addLabels({
-              owner: github.context.issue.owner,
-              repo: github.context.issue.repo,
-              issue_number: github.context.issue.number,
-              labels: [`@yt/type/${currentType.value.name}`]
-            })
-          }
-        } else {
-          if (response.status === 404) {
-            console.log(`Issue not found in YT with code ${response.status}`);
-            core.setFailed(`Issue ${issueId} not found in your Youtrack instance.`);
-          } else {
-            core.setFailed(`Unknown error connecting to youtrack ${response.status}`);
+  tickets.forEach(async id => await checkIssueExist(id));
+
+  await commentPR(
+    `Linked PR to issues:
+${tickets.map(id => `- [${id}](${getIssueLink(id)})`).join("\n")}`
+  );
+
+  console.log("Commented PR with linked issues.");
+
+  tickets.forEach(async issueId => {
+    await commentYT(
+      issueId,
+      `New PR [#${github.context.issue.number}](${PR_URL}) opened at [${github.context.issue.owner}/${github.context.issue.repo}](${REPO_URL}) by ${github.context.actor}.`
+    );
+  });
+
+  console.log(`Commented YT issues with the according PR.`);
+
+  await updatePR();
+
+  console.log("Updated PR description with YT links.");
+
+  tickets.forEach(async issueId => {
+    const fields = getFields(issueId);
+
+    const currentState = fields.find(x => x.name === "State");
+    const currentStateValue =
+      currentState &&
+      currentState.value &&
+      currentState.value.name.toLowerCase();
+
+    if (["to do", "to fix", "in progress"].some(x => x == currentStateValue)) {
+      const response = await ytApi.post(
+        `${issueId}/fields/${currentState.id}?fields=name,id,value(name)`,
+        {
+          value: {
+            name: "PR Open"
           }
         }
-      })
-      .catch(err => {
-        core.setFailed(err.message);
-      });
-    core.setOutput('issueId', issueId);
-  } 
-  catch (error) {
-    core.setFailed(error.message);
+      );
+
+      console.log(`Changed issue to PR Open ${response.status}`);
+
+      await commentPR(
+        `Issue [${issueId}](${getIssueLink(issueId)}) changed from *${
+          currentState.value.name
+        }* to *PR Open*`
+      );
+    }
+
+    const currentType = fields.find(x => x.name === "Type");
+
+    if (currentType && currentType.value && currentType.value.name) {
+      labelPR([`@yt/type/${currentType.value.name}`]);
+    }
+  });
+
+  core.setOutput("issues", tickets);
+}
+
+async function labelPR(labels) {
+  await octokit.issues.addLabels({
+    owner: github.context.issue.owner,
+    repo: github.context.issue.repo,
+    issue_number: github.context.issue.number,
+    labels
+  });
+}
+
+function getPrDescription() {
+  if (
+    github.context &&
+    github.context.payload &&
+    github.context.payload.pull_request
+  ) {
+    return github.context.payload.pull_request.description;
   }
 }
 
-const getPrTitle = () => {
-  if (github.context && github.context.payload && github.context.payload.pull_request) {
-    return github.context.payload.pull_request.title;
-  }
-};
+function getMatchingTickets() {
+  console.log(`Checking ${ISSUE_REGEX} against the PR description`);
 
-run();
+  const description = getPrDescription();
+  const matches = [...description.matchAll(ISSUE_REGEX)];
+
+  return matches.map(x => x[0]);
+}
+
+async function checkIssueExist(issueId) {
+  const response = await ytApi.get(`api/issues/${issueId}`);
+
+  if (response.status === 404) {
+    throw new Error(`Issue ${issueId} not found in your YouTrack instance.`);
+  } else if (response.statusText != "OK") {
+    throw new Error(`Unknown error connecting to YouTrack ${response.status}`);
+  }
+}
+
+async function commentPR(body) {
+  await octokit.issues.createComment({
+    owner: github.context.issue.owner,
+    repo: github.context.issue.repo,
+    issue_number: github.context.issue.number,
+    body
+  });
+}
+
+async function commentYT(issueId, text) {
+  await ytApi.post(`${issueId}/comments`, {
+    text,
+    usesMarkdown: true
+  });
+}
+
+async function updatePR() {
+  const description = getPrDescription();
+  description.replace(ISSUE_REGEX, ticket => `[${ticket}](${YT_URL}${ticket})`);
+
+  await octokit.pulls.update({
+    owner: github.context.issue.owner,
+    repo: github.context.issue.repo,
+    pull_number: github.context.pull.number,
+    body: description
+  });
+}
+
+async function getFields(issueId) {
+  const response = await ytApi.get(
+    `${issueId}/fields?fields=name,id,value(name)`
+  );
+
+  return response.data;
+}
+
+function getIssueLink(id) {
+  return `${YT_URL}issue/${id}`;
+}
+
+try {
+  run();
+} catch (error) {
+  core.setFailed(error.message);
+}
